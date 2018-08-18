535afb0b45a641409836244d8df86293351f3a58
PJ-Dev <olero1@gmail.com>
2018-08-18 16:48:21 -0600

    JS: Kelvin Weather

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-let myName = 'Peter';
-let myCity = 'Denver';
-
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+//Kelvin is always 293
+const kelvin = 293;
+//Celsius is 273degrees lower than Kelvin
+const celsius = kelvin-273;
+//converting C to F
+let fahrenheit = celsius*(9/5)+32;
+//putting a floor to the temp
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
