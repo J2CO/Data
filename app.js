8b4552863b7dfeb628c1fc263c5b5371a70542f4
PJ-Dev <olero1@gmail.com>
2018-08-24 16:54:39 -0600

    JS: Switch Statements

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let moonPhase = 'full';
-let isFoggyNight = false;
 
-if (moonPhase === 'full' || isFoggyNight === true) {
-  console.log('Howl!');
-} else if (moonPhase === 'mostly full') {
-  console.log('Arms & Legs are getting harrier!');
-} else if (moonPhase === 'mostly new') {
-  console.log('Back on two feet.');
-} else {
-  console.log('Invalid Moon Phase.');
-}
+switch (moonPhase) {
+  case 'full':
+    console.log('Howl!');
+    break;
+  case 'mostly full':
+    console.log('Arms & Legs are getting harrier!');
+    break;
+  case 'mostly new':
+    console.log('Back on two feet.');
+    break;
+  default:
+    console.log('Invalid Moon Phase');
+    break;
+} 
