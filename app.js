637e7af80cee03ca714bc98df76832b2ad73d873
PJ-Dev <olero1@gmail.com>
2018-08-24 18:48:30 -0600

    JS: Return

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+let orderCount = 0;
 const takeOrder = (topping, crustType) => {
   console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+  orderCount += 1;
 };
-
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+}
 takeOrder('mushroom', 'thin crust');
 takeOrder('spinach', 'whole wheat');
 takeOrder('pepperoni', 'brooklyn style');
