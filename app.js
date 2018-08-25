c1d7fc1b085fa72cd9d139286cfdc06456f54fda
PJ-Dev <olero1@gmail.com>
2018-08-24 18:28:23 -0600

    JS: Functions

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-let raceNumber = Math.floor(Math.random() * 1000);
-let registeredEarly = false;
-let runnerAge = 18;
-if(registeredEarly && runnerAge > 18) {
-  raceNumber += 1000;
+const takeOrder = () => {
+  console.log('Order: pizza')
 }
 
-if(registeredEarly && runnerAge > "18"){
-  console.log(`Your Race begins at 9:30am and your Number is: ${raceNumber}.`);
-}
-else if(!registeredEarly && runnerAge > "18"){
-  console.log(`Your Race begins at 11:00am and your Number is: ${raceNumber}.`);
-}
-else if(runnerAge < "18"){
-  console.log(`Your Race begins at 12:30pm and your Number is: ${raceNumber}.`);
-}
-else {
-  console.log('Please check-in at the Registration desk - thanks!');
-}
+takeOrder()
